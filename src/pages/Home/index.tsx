5a5df515da138a5bf5b18c0d6723972bae0298c1
Lee, Jong Won <138106799+javadocq@users.noreply.github.com>
2026-02-04 12:09:35 +0900

    Feat: 디자인 토큰 적용 (#2)
    * feat: design color
    
    * feat: typograph & alias
    
    * feat: add icon & svgr setting
    
    * docs: update styling.md

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import * as S from '@/pages/Home/Home.styles';
+import User from '@/assets/icons/circle_user.svg?react';
+
 export default function Home() {
   return (
-    <div>
+    <S.Container>
       <h1>Home</h1>
-    </div>
+      <User width={40} height={40} fill="red" />
+    </S.Container>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,150 @@
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: {
+      '00': '#5A5CF5',
+    },
+    secondary: {
+      '00': '#031786',
+      '01': '#0A209E',
+      '02': '#2135A8',
+      '03': '#29219C',
+      '04': '#394ECA',
+      '05': '#3D57E9',
+      '06': '#A5B8F1',
+      '07': '#D4D9F9',
+      '08': '#E5E8FA',
+      '09': '#F6F6FB',
+    },
+    grayScale: {
+      '00': '#0A0A0A',
+      '01': '#262630',
+      '02': '#4C4C54',
+      '03': '#525252',
+      '04': '#737373',
+      '05': '#8A8D92',
+      '06': '#A3A3A3',
+      '07': '#BABABA',
+      '08': '#DBDEE6',
+      '09': '#F0F0F3',
+      '10': '#FAFAFA',
+      '11': '#FDFDFD',
+    },
+    labelColor: {
+      '00': '#028B92',
+      '01': '#1FD0BC',
+      '02': '#DEF7F4',
+      '03': '#E06412',
+      '04': '#F18219',
+      '05': '#FBF6D8',
+      '06': '#DE2225',
+      '07': '#F78387',
+      '08': '#FFDCDC',
+    }
+  },
+  typography : {
+    display: {
+      'D0': {
+        fontSize: '40px',
+        fontWeight: '700',
+        lineHeight: '60px',
+      },
+    },
+    head: {
+      'H0': {
+        fontSize: '36px',
+        fontWeight: '800',
+        lineHeight: '54px',
+      },
+      'H1': {
+        fontSize: '36px',
+        fontWeight: '700',
+        lineHeight: '36px',
+      },
+      'H2': {
+        fontSize: '32px',
+        fontWeight: '700',
+        lineHeight: '48px',
+      },
+      'H3': {
+        fontSize: '28px',
+        fontWeight: '600',
+        lineHeight: '42px',
+      },
+      'H4': {
+        fontSize: '24px',
+        fontWeight: '600',
+        lineHeight: '36px',
+      },
+    },
+    body: {
+      'XL': {
+        fontSize: '24px',
+        fontWeight: '400',
+        lineHeight: '36px',
+      },
+      'L0': {
+        fontSize: '20px',
+        fontWeight: '400',
+        lineHeight: '30px',
+      },
+      'L1': {
+        fontSize: '20px',
+        fontWeight: '500',
+        lineHeight: '30px',
+      },
+      'L2': {
+        fontSize: '20px',
+        fontWeight: '700',
+        lineHeight: '30px',
+      },
+      'M0': {
+        fontSize: '16px',
+        fontWeight: '400',
+        lineHeight: '24px',
+      },
+      'M1': {
+        fontSize: '14px',
+        fontWeight: '400',
+        lineHeight: '21px',
+      },
+      'M2': {
+        fontSize: '14px',
+        fontWeight: '700',
+        lineHeight: '21px',
+      },
+      'S0': {
+        fontSize: '16px',
+        fontWeight: '400',
+        lineHeight: '24px',
+      },
+      'S1': {
+        fontSize: '16px',
+        fontWeight: '500',
+        lineHeight: '24px',
+      },
+      'S2': {
+        fontSize: '16px',
+        fontWeight: '600',
+        lineHeight: '24px',
+      },
+      'XS1': {
+        fontSize: '14px',
+        fontWeight: '400',
+        lineHeight: '21px',
+      },
+      'XS2': {
+        fontSize: '14px',
+        fontWeight: '600',
+        lineHeight: '21px',
+      },
+    },
+    caption: {
+      'C0': {
+        fontSize: '12px',
+        fontWeight: '500',
+        lineHeight: '18px',
+      },
+    },
   },
 };
 
